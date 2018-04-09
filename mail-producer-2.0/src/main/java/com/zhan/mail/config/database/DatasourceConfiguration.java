5ad3f2707fcacb080179bca3c3b4cb88be363c22
zhan <zhan@chenyunhandembp.lan>
2018-04-09 21:44:10 +0800

    自定义servlet filter

--- mail-producer-2.0/src/main/java/com/zhan/mail/config/database/DatasourceConfiguration.java
+++ mail-producer-2.0/src/main/java/com/zhan/mail/config/database/DatasourceConfiguration.java
@@ -7,14 +7,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 
 @Configuration
 public class DatasourceConfiguration {
 	
 	private static Logger LOGGER = LoggerFactory.getLogger(DatasourceConfiguration.class);
+	
 	
 	@Value("${druid.type}")
 	private Class<? extends DataSource> dataSourceType;
@@ -24,8 +30,37 @@ public class DatasourceConfiguration {
 	@ConfigurationProperties(prefix="druid.master")
 	public DataSource masterDataSource() {
 		DataSource masterDataSource = DataSourceBuilder.create().type(dataSourceType).build();
-		LOGGER.info("========MASTER: {}=========", masterDataSource);
+		LOGGER.info("========MASTER: {}=========",masterDataSource);
 		return masterDataSource;
 	}
+	
+	
+	@Bean(name="slaveDataSource")
+	public DataSource slaveDataSource() {
+		DataSource slaveDataSource = DataSourceBuilder.create().type(dataSourceType).build();
+		LOGGER.info("========SLAVE: {}=========",slaveDataSource);
+		return slaveDataSource;
+	}
 
+
+	@Bean
+	public ServletRegistrationBean druidServlet() {
+		ServletRegistrationBean reg = new ServletRegistrationBean();
+		reg.setServlet(new StatViewServlet());
+		reg.addInitParameter("allow", "127.0.0.1");
+		reg.addInitParameter("deny", "192.168.16.72");
+		reg.addUrlMappings("/druid/*");
+		LOGGER.info("========druidServlet: {}========",reg);
+		return reg;
+	}
+	
+	@Bean
+	public FilterRegistrationBean druidFilter() {
+		FilterRegistrationBean filter = new FilterRegistrationBean();
+		filter.setFilter(new WebStatFilter());
+		filter.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico");
+		filter.addUrlPatterns("/*");
+		LOGGER.info("========druidFilter: {}========",filter);
+		return filter;
+	}
 }
